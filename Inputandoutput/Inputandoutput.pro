480db284422592ddd9f230ce3cc119c8b4377745
Vivek95vs <55971500+Vivek95vs@users.noreply.github.com>
2026-04-30 09:35:50 +0530

    Add files via upload

---
+++ Inputandoutput/Inputandoutput.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2021-04-05T12:41:34
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Inputandoutput
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ Inputandoutput/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtCore>
+#include <QtGui>
+#include <QLabel>
+#include <QFileSystemModel>
+#include <QFileDialog>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+
+private slots:
+    void on_treeView_clicked(const QModelIndex &index);
+
+private:
+    Ui::MainWindow *ui;
+    QFileSystemModel *dirmodel;
+    QFileSystemModel *foldermodel;
+    QFileSystemModel *filemodel;
+};
+
+#endif // MAINWINDOW_H
