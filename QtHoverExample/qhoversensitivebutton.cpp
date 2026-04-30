9bbedfd313192157acf3066d01d2ad9d4027aa75
Vivek95vs <55971500+Vivek95vs@users.noreply.github.com>
2026-04-30 10:04:27 +0530

    Add files via upload

---
+++ QtHoverExample/qhoversensitivebutton.cpp
@@ -0,0 +1,53 @@
+#include "qhoversensitivebutton.h"
+
+QHoverSensitiveButton::QHoverSensitiveButton(QWidget *parent) : QGraphicsView(parent)
+{
+    setMouseTracking(true);
+    setAttribute(Qt::WA_Hover);
+}
+
+void QHoverSensitiveButton::hoverEnter(QHoverEvent *)
+{
+    QFont font = this->font();
+    font.setBold(true);
+    this->setFont(font);
+    repaint();
+}
+
+void QHoverSensitiveButton::hoverLeave(QHoverEvent *)
+{
+    QFont font = this->font();
+    font.setBold(false);
+    this->setFont(font);
+    repaint();
+}
+
+void QHoverSensitiveButton::hoverMove(QHoverEvent *)
+{
+    QFont font = this->font();
+    font.setBold(true);
+    this->setFont(font);
+    repaint();
+}
+
+bool QHoverSensitiveButton::event(QEvent *event)
+{
+    switch(event->type())
+    {
+    case QEvent::HoverEnter:
+        hoverEnter(static_cast<QHoverEvent*>(event));
+        return true;
+        break;
+    case QEvent::HoverLeave:
+        hoverLeave(static_cast<QHoverEvent*>(event));
+        return true;
+        break;
+    case QEvent::HoverMove:
+        hoverMove(static_cast<QHoverEvent*>(event));
+        return true;
+        break;
+    default:
+        break;
+    }
+    return QWidget::event(event);
+}
---
+++ QtHoverExample/ui_mainwindow.h
@@ -0,0 +1,73 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.5.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGraphicsView>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QGridLayout *gridLayout;
+    QGraphicsView *graphicsView;
+    QGraphicsView *graphicsView_2;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(1053, 786);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        gridLayout = new QGridLayout(centralWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        graphicsView = new QGraphicsView(centralWidget);
+        graphicsView->setObjectName(QStringLiteral("graphicsView"));
+
+        gridLayout->addWidget(graphicsView, 1, 0, 1, 1);
+
+        graphicsView_2 = new QGraphicsView(centralWidget);
+        graphicsView_2->setObjectName(QStringLiteral("graphicsView_2"));
+
+        gridLayout->addWidget(graphicsView_2, 2, 0, 1, 1);
+
+        MainWindow->setCentralWidget(centralWidget);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Hover Sensitive Push Button", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
